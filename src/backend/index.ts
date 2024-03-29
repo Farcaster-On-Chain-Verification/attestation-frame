a362bf624bfa363b5b5d1a2bda7f7e80408409e7
Gonzalo Melo <gonzalomelov@gmail.com>
2024-03-29 17:58:14 -0300

    Make it async

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -25,9 +25,28 @@ interface FarcasterMessage {
 export default Server(() => {
     const app = express();
 
+    const NEYNAR_API_KEY = 'xxx';
+    const FARCASTER_ATTESTOR = 'xxx';
+
     app.use(express.json());
 
+    app.get('/', (req, res) => {
+        res.send(pageFromTemplate(
+            'https://ipfs.io/ipfs/QmUF9BmteniwSsLco3XaYVGbpdh3FWuUW8CqbDP52azE65',
+            'Attest',
+            'https://4c8a-186-55-232-157.ngrok-free.app/action',
+            mainPageBody
+        ))
+    });
+
     app.post('/action', async (req, res) => {
+        res.send(pageFromTemplate(
+            'https://ipfs.io/ipfs/QmawGYH6TdvxsC1zhMXtAPJcjy7R5yCMQ6SBmUrwGD5pNE',
+            'Refresh',
+            'https://4c8a-186-55-232-157.ngrok-free.app/refresh',
+            mainPageBody
+        ))
+
         const body = req.body as FarcasterMessage;
         
         const buttonChosen = body.untrustedData.buttonIndex;
@@ -41,7 +60,7 @@ export default Server(() => {
             method: 'POST',
             headers: {
                 accept: 'application/json',
-                api_key: 'xxx',
+                api_key: NEYNAR_API_KEY,
                 'content-type': 'application/json'
             },
             body: JSON.stringify({
@@ -63,9 +82,8 @@ export default Server(() => {
         console.log('fid', fid);
         console.log('eth_addresses', eth_addresses[1]);
 
-        
         const wallet = new ethers.Wallet(
-            "xxx",
+            FARCASTER_ATTESTOR,
             ethers.getDefaultProvider('https://sepolia.base.org') // chainId: "eip155:84532",
         );
 
@@ -80,52 +98,43 @@ export default Server(() => {
 
         console.log(`encoded: ${encoded}`);
 
-        try {
-            const info = {
-                schema: "0xca168d038c5d527bb9724b3201f026520b498d334a2f3f446181d6420d7fb515",
-                data: {
-                    recipient: eth_addresses[1],
-                    data: encoded,
-                    expirationTime: BigInt(0),
-                    revocable: false,
-                    refUID: ethers.ZeroHash,
-                    value: BigInt(0)
-                }
-            };
-            
-            console.log(`info`, info.data.refUID);
-    
-            const contract = new ethers.Contract("0x4200000000000000000000000000000000000021", abi.abi, wallet);
-    
-            const tx = await contract.attest(info);
-    
-            console.log(`tx: ${tx}`);
-    
-            // Wait for the transaction to be mined
-            await tx.wait();
-    
-            console.log(`Transaction sent with hash: ${tx.hash}`);
-        } catch (err) {
-            console.log(err);
-        }
+        const info = {
+            schema: "0xca168d038c5d527bb9724b3201f026520b498d334a2f3f446181d6420d7fb515",
+            data: {
+                recipient: eth_addresses[1],
+                data: encoded,
+                expirationTime: BigInt(0),
+                revocable: false,
+                refUID: ethers.ZeroHash,
+                value: BigInt(0)
+            }
+        };
         
+        console.log(`info`, info.data.refUID);
 
-        res.send(pageFromTemplate(
-            'https://raw.githubusercontent.com/vrypan/farcaster-brand/main/icons/icon-rounded/purple-white.png',
-            buttonTexts[0],
-            'https://4c8a-186-55-232-157.ngrok-free.app/action',
-            mainPageBody
-        ))
+        const contract = new ethers.Contract("0x4200000000000000000000000000000000000021", abi.abi, wallet);
+
+        const attestInit = new Date();
+        const tx = await contract.attest(info);
+        const attestEnd = new Date();
+        const attestDuration = attestInit.getTime() - attestEnd.getTime();
+        console.log(`Attest duration: ${attestDuration} ms`);
+
+        const transactionMinedInit = new Date();
+        await tx.wait();
+        const transactionMinedEnd = new Date();
+        const transactionMinedDuration = transactionMinedInit.getTime() - transactionMinedEnd.getTime();
+        console.log(`transactionMined duration: ${transactionMinedDuration} ms`);
+
+        console.log(`Transaction sent with hash: ${tx.hash}`);
     });
 
-    app.get('/', (req, res) => {
-        res.send(pageFromTemplate(
-            'https://raw.githubusercontent.com/vrypan/farcaster-brand/main/icons/icon-rounded/purple-white.png',
-            'Button 1',
-            'https://4c8a-186-55-232-157.ngrok-free.app/action',
+    app.post('/refresh', async (req, res) => {
+        res.send(finalFrame(
+            'https://ipfs.io/ipfs/QmeJ7JRpo15yGHjPj6bjxdda1y96GEctLUXNkTTtX8MUMT',
             mainPageBody
         ))
-    });
+    })
 
     return app.listen();
 });
@@ -167,6 +176,31 @@ let pageFromTemplate = (
     <meta property='og:title' content='Azle farcaster frame' />
     <meta property='og:image' content='${imageUrl}' />
     <title>Azle farcaster frame</title>
+</head>
+
+<body>
+    ${body}
+</body>
+
+</html>
+`;
+
+let finalFrame = (
+    imageUrl: string,
+    body: string
+) => `
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+    <meta charset='utf-8' />
+    <meta name='viewport' content='width=device-width, initial-scale=1' />
+    <meta name='next-size-adjust' />
+    <meta property='fc:frame' content='vNext' />
+    <meta property='fc:frame:image' content='${imageUrl}' />
+    <meta property='og:title' content='Attestation Done!' />
+    <meta property='og:image' content='${imageUrl}' />
+    <title>Attestation Done!</title>
 </head>
 
 <body>
