27c0426ac6ad7588dfc4e048ba91f9203b374c3c
Gonzalo Melo <gonzalomelov@gmail.com>
2024-04-01 08:10:56 -0300

    Remove EAS attest from the decentralized service

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,8 +1,5 @@
-import { Server, jsonStringify } from 'azle';
-import { ethers } from 'ethers';
+import { ic, Server, jsonStringify } from 'azle';
 import express from 'express';
-import abi from "../../abi.json";
-import { SchemaEncoder } from "@ethereum-attestation-service/eas-sdk";
 
 interface FarcasterMessage {
     untrustedData: {
@@ -26,9 +23,7 @@ export default Server(() => {
     const app = express();
 
     const NEYNAR_API_KEY = 'xxx';
-    const FARCASTER_ATTESTOR = 'xxx';
-    const DOMAIN = 'https://she77-3iaaa-aaaap-ag7ba-cai.raw.icp0.io';
-    // const DOMAIN = 'https://f4ba-186-55-232-157.ngrok-free.app';
+    const SERVER = 'https://attest-theta.vercel.app';
 
     app.use(express.json());
 
@@ -36,160 +31,107 @@ export default Server(() => {
         res.send(pageFromTemplate(
             'https://ipfs.io/ipfs/QmUF9BmteniwSsLco3XaYVGbpdh3FWuUW8CqbDP52azE65',
             'Attest',
-            `${DOMAIN}/action`,
+            `${SERVER}/api/action`,
             mainPageBody
         ))
-    });
-
-    app.post('/action', async (req, res) => {
-        res.send(pageFromTemplate(
-            'https://ipfs.io/ipfs/QmawGYH6TdvxsC1zhMXtAPJcjy7R5yCMQ6SBmUrwGD5pNE',
-            'Refresh',
-            `${DOMAIN}/refresh`,
-            mainPageBody
-        ))
-
-        const body = req.body as FarcasterMessage;
-        
-        const response = await fetch('https://api.neynar.com/v2/farcaster/frame/validate', {
-            method: 'POST',
-            headers: {
-                accept: 'application/json',
-                api_key: NEYNAR_API_KEY,
-                'content-type': 'application/json'
-            },
-            body: jsonStringify({
-                cast_reaction_context: true,
-                follow_context: true,
-                signer_context: true,
-                message_bytes_in_hex: body.trustedData.messageBytes
-            })
-        });
-        
-        const trustedData = await response.json();
-        const { action: { interactor: { fid, verified_addresses: { eth_addresses } } } } = trustedData;
-
-        console.log('fid', fid);
-        console.log('eth_addresses', eth_addresses[1]);
-
-        // Let the user choose which wallet to use
-
-        // Recast and follow
-
-        const wallet = new ethers.Wallet(
-            FARCASTER_ATTESTOR,
-            ethers.getDefaultProvider('https://sepolia.base.org') // chainId: "eip155:84532",
-        );
-
-        console.log(`Wallet: ${wallet.address}`);
-
-        const schemaEncoder = new SchemaEncoder("uint32 fid,uint64 timestamp");
-
-        const encoded = schemaEncoder.encodeData([
-          { name: "fid", type: "uint32", value: fid },
-          { name: "timestamp", type: "uint64", value: Date.now() },
-        ]);
-
-        console.log(`encoded: ${encoded}`);
-
-        const info = {
-            schema: "0xca168d038c5d527bb9724b3201f026520b498d334a2f3f446181d6420d7fb515",
-            data: {
-                recipient: eth_addresses[1],
-                data: encoded,
-                expirationTime: BigInt(0),
-                revocable: false,
-                refUID: ethers.ZeroHash,
-                value: BigInt(0)
-            }
-        };
-
-        const contract = new ethers.Contract("0x4200000000000000000000000000000000000021", abi.abi, wallet);
-
-        const tx = await contract.attest(info);
-
-        console.log(`Transaction sent with hash: ${tx.hash}`);
-
-        const receipt = await tx.wait();
-
-        const uid = receipt.logs[0].data;
-
-        console.log(`uid`, uid);
     });
 
     app.post('/refresh', async (req, res) => {
         console.log('refresh');
 
-        const body = req.body as FarcasterMessage;
+        try {
 
-        const response = await fetch('https://api.neynar.com/v2/farcaster/frame/validate', {
-            method: 'POST',
-            headers: {
-                accept: 'application/json',
-                api_key: NEYNAR_API_KEY,
-                'content-type': 'application/json'
-            },
-            body: jsonStringify({
-                cast_reaction_context: true,
-                follow_context: true,
-                signer_context: true,
-                message_bytes_in_hex: body.trustedData.messageBytes
-            })
-        });
-        
-        const trustedData = await response.json();
-        const { action: { interactor: { fid, verified_addresses: { eth_addresses } } } } = trustedData;
+            ic.setOutgoingHttpOptions({
+                cycles: 20852722800n
+            });
 
-        console.log('fid', fid);
-        console.log('eth_addresses', eth_addresses[1]);
+            const body = req.body as FarcasterMessage;
 
-        // const response = await fetch("https://base-sepolia.easscan.org/graphql", {
-        //     method: 'POST',
-        //     headers: {
-        //         accept: 'application/json',
-        //         'content-type': 'application/json'
-        //     },
-        //     body: jsonStringify({
-        //         query: 'query Attestations($where: AttestationWhereInput, $orderBy: [AttestationOrderByWithRelationInput!]) {\n  attestations(where: $where, orderBy: $orderBy) {\n    attester\n    revocationTime\n    expirationTime\n    time\n    recipient\n    id\n    data\n  }\n}',
-        //         variables: {
-        //             where: {
-        //                 schemaId: {
-        //                     equals: '0xca168d038c5d527bb9724b3201f026520b498d334a2f3f446181d6420d7fb515',
-        //                 },
-        //                 OR: [
-        //                     {
-        //                     attester: {
-        //                         equals: '0x372082138ea420eBe56078D73F0359D686A7E981',
-        //                     },
-        //                     },
-        //                     {
-        //                     recipient: {
-        //                         equals: '0x372082138ea420eBe56078D73F0359D686A7E981',
-        //                     },
-        //                     },
-        //                 ],
-        //             },
-        //             orderBy: [
-        //                 {
-        //                     time: "desc",
-        //                 },
-        //             ],
-        //         },
-        //     })
-        // });
-        
-        // console.log('response');
+            const response = await fetch('https://api.neynar.com/v2/farcaster/frame/validate', {
+                method: 'POST',
+                headers: {
+                    accept: 'application/json',
+                    api_key: NEYNAR_API_KEY,
+                    'content-type': 'application/json'
+                },
+                body: jsonStringify({
+                    cast_reaction_context: true,
+                    follow_context: true,
+                    signer_context: true,
+                    message_bytes_in_hex: body.trustedData.messageBytes
+                })
+            });
+            
+            const trustedData = await response.json();
+            const { action: { interactor: { fid, verified_addresses: { eth_addresses } } } } = trustedData;
 
-        // const { data: { attestations } } = await response.json();
+            console.log('fid', fid);
+            console.log('eth_addresses', eth_addresses[1]);
 
-        // console.log('attestations', attestations);
+            // const response = await fetch("https://base-sepolia.easscan.org/graphql", {
+            //     method: 'POST',
+            //     headers: {
+            //         accept: 'application/json',
+            //         'content-type': 'application/json'
+            //     },
+            //     body: jsonStringify({
+            //         query: 'query Attestations($where: AttestationWhereInput, $orderBy: [AttestationOrderByWithRelationInput!]) {\n  attestations(where: $where, orderBy: $orderBy) {\n    attester\n    revocationTime\n    expirationTime\n    time\n    recipient\n    id\n    data\n  }\n}',
+            //         variables: {
+            //             where: {
+            //                 schemaId: {
+            //                     equals: '0xca168d038c5d527bb9724b3201f026520b498d334a2f3f446181d6420d7fb515',
+            //                 },
+            //                 OR: [
+            //                     {
+            //                     attester: {
+            //                         equals: '0x372082138ea420eBe56078D73F0359D686A7E981',
+            //                     },
+            //                     },
+            //                     {
+            //                     recipient: {
+            //                         equals: '0x372082138ea420eBe56078D73F0359D686A7E981',
+            //                     },
+            //                     },
+            //                 ],
+            //             },
+            //             orderBy: [
+            //                 {
+            //                     time: "desc",
+            //                 },
+            //             ],
+            //         },
+            //     })
+            // });
+            
+            // console.log('response');
 
-        res.send(pageWithLinkFromTemplate(
-            'https://ipfs.io/ipfs/QmeJ7JRpo15yGHjPj6bjxdda1y96GEctLUXNkTTtX8MUMT',
-            'See Attestation on EAS',
-            `https://base-sepolia.easscan.org/address/${eth_addresses[1]}`,  // `https://base-sepolia.easscan.org/attestation/view/${attestation.id}`,
+            // const { data: { attestations } } = await response.json();
+
+            // console.log('attestations', attestations);
+
+            res.send(pageWithLinkFromTemplate(
+                'https://ipfs.io/ipfs/QmeJ7JRpo15yGHjPj6bjxdda1y96GEctLUXNkTTtX8MUMT',
+                'See Attestation on EAS',
+                `https://base-sepolia.easscan.org/address/${eth_addresses[1]}`,  // `https://base-sepolia.easscan.org/attestation/view/${attestation.id}`,
+                mainPageBody
+            ))
+
+        } catch (err) {
+          let message = '';
+          if (typeof err === "string") {
+            message = err;
+          } else if (err instanceof Error) {
+            message = err.message;
+          }
+      
+          res.send(pageFromTemplate(
+            'https://ipfs.io/ipfs/QmawGYH6TdvxsC1zhMXtAPJcjy7R5yCMQ6SBmUrwGD5pNE',
+            message,
+            `https://s3afo-miaaa-aaaap-ag7da-cai.raw.icp0.io/refresh`,
             mainPageBody
-        ))
+          ))
+
+        }
     })
 
     return app.listen();
@@ -210,7 +152,6 @@ const mainPageBody = `
         </p>
     </div>
 `;
-
 
 let pageFromTemplate = (
     imageUrl: string,
