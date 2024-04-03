8d9de98a1eb6362e9bf444a873f6032aea3cef4a
Gonzalo Melo <gonzalomelov@gmail.com>
2024-04-03 12:31:27 -0300

    Indent

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,57 +1,61 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-const ATTEST_SERVER = 'https://attest-theta.vercel.app';
+const ATTEST_SERVER = "https://attest-theta.vercel.app";
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send(pageFromTemplate(
-        'https://ipfs.io/ipfs/QmUF9BmteniwSsLco3XaYVGbpdh3FWuUW8CqbDP52azE65',
-        'Attest',
-        `${ATTEST_SERVER}/api/action`,
-        mainPageBody
-    ))
+app.get("/", (req, res) => {
+  res.send(
+    pageFromTemplate(
+      "https://ipfs.io/ipfs/QmUF9BmteniwSsLco3XaYVGbpdh3FWuUW8CqbDP52azE65",
+      "Attest",
+      `${ATTEST_SERVER}/api/action`,
+      mainPageBody
+    )
+  );
 });
 
-app.post('/refresh', async (req, res) => {
-    console.log('refresh');
+app.post("/refresh", async (req, res) => {
+  console.log("refresh");
 
-    try {
-        const address = req.query.address;
-        
-        console.log('address', address);
+  try {
+    const address = req.query.address;
 
-        // const uid = req.query.uid;
-        
-        // console.log('uid', uid);
+    console.log("address", address);
 
-        res.send(pageWithLinkFromTemplate(
-            'https://ipfs.io/ipfs/QmeJ7JRpo15yGHjPj6bjxdda1y96GEctLUXNkTTtX8MUMT',
-            'See Attestation on EAS',
-            `https://base-sepolia.easscan.org/address/${address}`,
-            // `https://base-sepolia.easscan.org/attestation/view/${uid}`,
-            mainPageBody
-        ))
+    // const uid = req.query.uid;
 
-    } catch (err) {
-        let message = '';
-        if (typeof err === "string") {
-        message = err;
-        } else if (err instanceof Error) {
-        message = err.message;
-        }
-    
-        res.send(pageFromTemplate(
-        'https://ipfs.io/ipfs/QmawGYH6TdvxsC1zhMXtAPJcjy7R5yCMQ6SBmUrwGD5pNE',
+    // console.log('uid', uid);
+
+    res.send(
+      pageWithLinkFromTemplate(
+        "https://ipfs.io/ipfs/QmeJ7JRpo15yGHjPj6bjxdda1y96GEctLUXNkTTtX8MUMT",
+        "See Attestation on EAS",
+        `https://base-sepolia.easscan.org/address/${address}`,
+        // `https://base-sepolia.easscan.org/attestation/view/${uid}`,
+        mainPageBody
+      )
+    );
+  } catch (err) {
+    let message = "";
+    if (typeof err === "string") {
+      message = err;
+    } else if (err instanceof Error) {
+      message = err.message;
+    }
+
+    res.send(
+      pageFromTemplate(
+        "https://ipfs.io/ipfs/QmawGYH6TdvxsC1zhMXtAPJcjy7R5yCMQ6SBmUrwGD5pNE",
         message,
         `https://s3afo-miaaa-aaaap-ag7da-cai.raw.icp0.io/refresh`,
         mainPageBody
-        ))
-
-    }
-})
+      )
+    );
+  }
+});
 
 app.listen();
 
@@ -72,10 +76,10 @@ const mainPageBody = `
 `;
 
 let pageFromTemplate = (
-    imageUrl: string,
-    button1Text: string,
-    apiUrl: string,
-    body: string
+  imageUrl: string,
+  button1Text: string,
+  apiUrl: string,
+  body: string
 ) => `
 <!DOCTYPE html>
 <html lang='en'>
@@ -101,10 +105,10 @@ let pageFromTemplate = (
 `;
 
 let pageWithLinkFromTemplate = (
-    imageUrl: string,
-    button1Text: string,
-    apiUrl: string,
-    body: string
+  imageUrl: string,
+  button1Text: string,
+  apiUrl: string,
+  body: string
 ) => `
 <!DOCTYPE html>
 <html lang='en'>
@@ -130,10 +134,7 @@ let pageWithLinkFromTemplate = (
 </html>
 `;
 
-let finalFrame = (
-    imageUrl: string,
-    body: string
-) => `
+let finalFrame = (imageUrl: string, body: string) => `
 <!DOCTYPE html>
 <html lang='en'>
 
